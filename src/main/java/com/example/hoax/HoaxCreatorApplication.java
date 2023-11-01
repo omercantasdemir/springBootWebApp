faea9f5afd6e9306575a062d96cc8d8dd205c60d
Ömercan Taşdemir <omercantasdemir@hotmail.com>
2023-11-01 17:20:23 +0300

    intro

---
+++ src/main/java/com/example/hoax/HoaxCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoaxCreatorApplication.class, args);
+    }
+//asd
+}
---
+++ src/test/java/com/example/hoax/HoaxCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoaxCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
