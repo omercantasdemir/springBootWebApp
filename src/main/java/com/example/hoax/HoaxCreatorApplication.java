60d611e0faf433fbad238dc72ad66c9548ae0180
Ömercan Taşdemir <omercantasdemir@hotmail.com>
2023-11-07 17:27:40 +0300

    user endpoint contunie

--- src/main/java/com/example/hoax/HoaxCreatorApplication.java
+++ src/main/java/com/example/hoax/HoaxCreatorApplication.java
@@ -2,8 +2,9 @@ package com.example.hoax;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HoaxCreatorApplication {
 
     public static void main(String[] args) {
